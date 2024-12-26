fbdd906a8bc7aa1ddee8dcd41677166c3fabf715
Santiago019-sketch <jemalagegidze@gmail.com>
2024-12-26 17:43:58 +0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,7 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "MidnightV5",
+Text = "Updated Script Loading Now Wait 3 Seconds Also Sorry For The Wait!",
+Duration = 3
+})
+Wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaosscripter/Riotscripter/refs/heads/main/Dragon%20Blox", true))()
